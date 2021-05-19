90570be61a35ace5549ea3b2cb0afedc86897b80
Dinaisbaih <isbaihdina.f@outlook.com>
2021-05-19 18:10:46 +0300

    task done

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,9 +9,11 @@
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
 function getOdds(numbers) {
-  // Your code here
+  
+  const oddNumbers = numbers.filter(function(num){return num % 2 !== 0;});
+  return oddNumbers;
 }
-
+console.log(getOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 /**
  * getEvens(numbers):
  * - receives an array of numbers called `numbers`
@@ -23,9 +25,11 @@ function getOdds(numbers) {
  * getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [52, 14, 56, 888]
  */
 function getEvens(numbers) {
-  // Your code here
+  
+  const evenNumbers = numbers.filter(function(num){return num % 2 === 0;});
+  return evenNumbers;
 }
-
+console.log(getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]))
 /**
  * countOccurences(x, numbers):
  * - receives a number `x`, and an array of numbers called `numbers`
@@ -36,9 +40,14 @@ function getEvens(numbers) {
  * countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
 function countOccurences(x, numbers) {
-  // Your code here
+  let count = 0;
+  numbers.map(function(num){
+    if(num === x){
+  count += 1
+    }
+  })
+  return count;
 }
-
 /**
  * makeThemDoctors(students):
  * - receives array `students`
@@ -49,7 +58,14 @@ function countOccurences(x, numbers) {
  */
 
 function makeThemDoctors(students) {
-  // Your code here
+
+ const doctors = students.map(function(student)  {
+    return `Dr. ${student}`
+  })
+
+  return doctors
+  
 }
+console.log(makeThemDoctors(["Ali", "Aseel", "Richard"]))
 
 module.exports = { getOdds, getEvens, countOccurences, makeThemDoctors };
